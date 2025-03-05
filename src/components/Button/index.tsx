9c2fdd5e882daf48c34bce4d30e5990cb376115d
zzmarques <walacemarqs@gmail.com>
2025-03-05 18:45:58 -0300

    criando segundo card do slide

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-
-
-const Button = () => {
-    return (
-        <button className='btn'>Rental car</button>
-    )
-}
-
-export default Button;
--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -1,6 +1,6 @@
-import img from '../../../public/img/car.png'
-import '../../sass/components/_Slide.scss'
-import Button from '../Button'
+import car1 from '../../../public/img/car.png';
+// import car2 from '../../../public/img/car2.png';
+import '../../sass/components/_Slide.scss';
 
 export const Slide = () => {
     return (
@@ -12,13 +12,28 @@ export const Slide = () => {
                         <span className='title'>The Best Platform for Car Rental</span>
                         <span className='subtitle'>Ease of doing a car rental safely and reliably. Of course at a low price.</span>
                     </div>
-                    <Button/>
+                    <button className='btn ads1'>Rental car</button>
                 </div>
 
                 <div className='container-img'>
-                    <img src={img} alt="carro" />
+                    <img src={car1} alt="carro" />
                 </div>
             </div>
+
+            {/* <div className="slide-car2">
+
+                <div className='container-infos'>
+                    <div className='container-title'>
+                        <span className='title'>Easy way to rent a car at a low price</span>
+                        <span className='subtitle'>Providing cheap car rental services and safe and comfortable facilities.</span>
+                    </div>
+                    <button className='btn ads2'>Rental car</button>
+                </div>
+
+                <div className='container-img'>
+                    <img src={car2} alt="carro" />
+                </div>
+            </div> */}
         </section>
     )
 }
