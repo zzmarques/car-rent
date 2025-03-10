27c9febb073168d7c3e67fd963c76bd34cc4f5d6
zzmarques <walacemarqs@gmail.com>
2025-03-10 09:45:37 -0300

    criando a estilização do componente Popular

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import Container from './components/Container'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Pickup from './components/Pickup'
+import Popular from './components/Popular'
 import { Slide } from './components/Slide'
 import './sass/main.scss'
 
 function App() {
 
   return (
-    <>
+    <div>
       <Header/>
         
       <Container>
         <Slide/>
         <Pickup/>
+        <Popular/>
       </Container>
 
       <Footer/>
-    </>
+    </div>
   )
 }
 
--- src/components/Popular/index.tsx
+++ src/components/Popular/index.tsx
@@ -2,6 +2,8 @@ import { GoHeartFill, GoHeart } from "react-icons/go";
 import { MdPeopleAlt } from "react-icons/md";
 import { FaGasPump } from "react-icons/fa6";
 import { RiSteeringFill } from "react-icons/ri";
+import img from "../../../public/img/car.png";
+import "../../sass/components/_Popular.scss";
 
 const Popular = () => {
     return (
@@ -12,48 +14,129 @@ const Popular = () => {
                 <a href="#"><span>View All</span></a>
             </div>
 
-            <div className="card">
-
-                <div className="card-content">
-                    <div className="card-cabecalho">
-                        <div className="container-name">
-                            <span className="name">Koenigsegg</span>
-                            <span className="type">Sport</span>
-                        </div>
-                        <GoHeartFill />
-                    </div>
-
-                    <div className="container-img">
-                        <img src="" alt="carro" />
-                    </div>
-
-                    <div className="container-details">
-                        <div className="details">
-                            <FaGasPump />
-                            <span>90L</span>
+            <div className="container-cards">
+                <div className="card">
+                    
+                        <div className="card-cabecalho">
+                            <div className="container-name">
+                                <span className="name">Koenigsegg</span>
+                                <span className="type">Sport</span>
+                            </div>
+                            <GoHeartFill />
                         </div>
 
-                        <div className="details">
-                            <RiSteeringFill />
-                            <span>Manual</span>
+                        <div className="container-img">
+                            <img src={img} alt="carro" />
                         </div>
 
-                        <div className="details">
-                            <MdPeopleAlt />
-                            <span>2 People</span>
+                        <div className="container-details">
+                            <div className="details">
+                                <FaGasPump />
+                                <span>90L</span>
+                            </div>
+
+                            <div className="details">
+                                <RiSteeringFill />
+                                <span>Manual</span>
+                            </div>
+
+                            <div className="details">
+                                <MdPeopleAlt />
+                                <span>2 People</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="container-price">
-                        <span className="price">
-                            $99.00/<span className="des">day</span>
-                        </span>
+                        <div className="container-price">
+                            <span className="price">
+                                $99.00/<span className="des">day</span>
+                            </span>
 
-                        <button className="btn btn-card">Rental Now</button>
-                    </div>
+                            <button className="btn-card">Rental Now</button>
+                        </div>
+
                 </div>
 
+                <div className="card">
+                    
+                        <div className="card-cabecalho">
+                            <div className="container-name">
+                                <span className="name">Koenigsegg</span>
+                                <span className="type">Sport</span>
+                            </div>
+                            <GoHeartFill />
+                        </div>
 
+                        <div className="container-img">
+                            <img src={img} alt="carro" />
+                        </div>
+
+                        <div className="container-details">
+                            <div className="details">
+                                <FaGasPump />
+                                <span>90L</span>
+                            </div>
+
+                            <div className="details">
+                                <RiSteeringFill />
+                                <span>Manual</span>
+                            </div>
+
+                            <div className="details">
+                                <MdPeopleAlt />
+                                <span>2 People</span>
+                            </div>
+                        </div>
+
+                        <div className="container-price">
+                            <span className="price">
+                                $99.00/<span className="des">day</span>
+                            </span>
+
+                            <button className="btn-card">Rental Now</button>
+                        </div>
+
+                </div>
+
+                <div className="card">
+                    
+                        <div className="card-cabecalho">
+                            <div className="container-name">
+                                <span className="name">Koenigsegg</span>
+                                <span className="type">Sport</span>
+                            </div>
+                            <GoHeart />
+                        </div>
+
+                        <div className="container-img">
+                            <img src={img} alt="carro" />
+                        </div>
+
+                        <div className="container-details">
+                            <div className="details">
+                                <FaGasPump />
+                                <span>90L</span>
+                            </div>
+
+                            <div className="details">
+                                <RiSteeringFill />
+                                <span>Manual</span>
+                            </div>
+
+                            <div className="details">
+                                <MdPeopleAlt />
+                                <span>2 People</span>
+                            </div>
+                        </div>
+
+                        <div className="container-price">
+                            <span className="price">
+                                $99.00/<span className="des">day</span>
+                            </span>
+
+                            <button className="btn-card">Rental Now</button>
+                        </div>
+
+                </div>
             </div>
         </section>
     )
