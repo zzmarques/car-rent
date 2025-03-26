4dcefce6631b92b035ff8dc9e5b39b7aa9411f77
zzmarques <walacemarqs@gmail.com>
2025-03-26 09:55:12 -0300

    fazendo os formularios da page payment

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,6 +1,9 @@
 import { HiStar, HiOutlineStar } from "react-icons/hi";
 import carV1 from '../../../public/img/view1.png'
 import "../../sass/pages/_Payment.scss";
+import visa from "../../assets/imgs/Visa.png";
+import paypal from "../../assets/imgs/PayPal.png";
+import bitcoin from "../../assets/imgs/Bitcoin.png";
 
 const Payment = () => {
     return (
@@ -73,83 +76,176 @@ const Payment = () => {
 
                 </div>
 
-                <section aria-labelledby="billing-info">
-                    <h2 id="billing-info">Billing Info</h2>
-                    <p>Por favor, insira suas informações de cobrança.</p>
-                    <form>
-                        <label >Nome</label>
-                        <input type="text" id="name" name="name" placeholder="Seu nome" required/>
+                <section className="container-steps">
+
+                    <div className="header-step">
+                        <div className="header-form">
+                            <h2 className="title">Billing Info</h2>
+                            <span className="subtitle">Please enter your billing info</span>
+                        </div>
+
+                        <span className="steps">Step 1 of 4</span>
+                    </div>
+
+                    <form className="form-step">
+                        <div className="container-inputs">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" id="name" name="name" placeholder="Your name"/>
+                        </div>
                         
-                        <label >Endereço</label>
-                        <input type="text" id="address" name="address" placeholder="Endereço" required/>
+                        <div className="container-inputs">
+                            <label htmlFor="address">Address</label>
+                            <input type="text" id="address" name="address" placeholder="Address"/>
+                        </div>
                         
-                        <label >Telefone</label>
-                        <input type="tel" id="phone" name="phone" placeholder="Número de telefone" required/>
+                        <div className="container-inputs">
+                            <label htmlFor="phone">Phone Number</label>
+                            <input type="tel" id="phone" name="phone" placeholder="Phone Number"/>
+                        </div>
                         
-                        <label >Cidade</label>
-                        <input type="text" id="city" name="city" placeholder="Cidade" required/>
+                        <div className="container-inputs">
+                            <label htmlFor="city">Town/city</label>
+                            <input type="text" id="city" name="city" placeholder="Town or city"/>
+                        </div>
                     </form>
                 </section>
 
-                <section aria-labelledby="rental-info">
-                    <h2 id="rental-info">Rental Info</h2>
-                    <p>Por favor, selecione a data do seu aluguel.</p>
-                    <form>
-                        <fieldset>
-                            <legend>Pick-Up</legend>
-                            <label >Localização</label>
-                            <select id="pickup-location" name="pickup-location" required>
-                                <option value="">Selecione sua cidade</option>
-                            </select>
-                            <label >Horário</label>
-                            <input type="time" id="pickup-time" name="pickup-time" required/>
-                            <label >Data</label>
-                            <input type="date" id="pickup-date" name="pickup-date" required/>
-                        </fieldset>
+                <section className="container-steps">
+                    <div className="header-step">
+                        <div className="header-form">
+                            <h2 className="title">Rental Info</h2>
+                            <span className="subtitle">Please select your rental date</span>
+                        </div>
 
-                        <fieldset>
-                            <legend>Drop-Off</legend>
-                            <label >Localização</label>
-                            <select id="dropoff-location" name="dropoff-location" required>
-                                <option value="">Selecione sua cidade</option>
-                            </select>
-                            <label>Horário</label>
-                            <input type="time" id="dropoff-time" name="dropoff-time" required/>
-                            <label >Data</label>
-                            <input type="date" id="dropoff-date" name="dropoff-date" required/>
-                        </fieldset>
-                        </form>
+                        <span className="steps">Step 2 of 4</span>
+                    </div>
+
+                    <form className="form-step form-stap-2">
+
+                        <div className="container-pick-and-drop">
+
+                            <div className="container-inputs container-input-radio">
+                                <input type="radio" name="pick" id="pick"/>
+                                <label htmlFor="pick">Pick - Up</label>
+                            </div>
+
+                            <div className="container-inputs">
+                                <label htmlFor="pickup-location">Localização</label>
+                                <select id="pickup-location" name="pickup-location">
+                                    <option value="">Selecione sua cidade</option>
+                                </select>
+                            </div>
+
+                            <div className="container-inputs">
+                                <label htmlFor="pickup-time">Horário</label>
+                                <input type="time" id="pickup-time" name="pickup-time"/>
+                            </div>
+
+                            <div className="container-inputs">
+                                <label htmlFor="pickup-date">Data</label>
+                                <input type="date" id="pickup-date" name="pickup-date"/>
+                            </div>
+                        </div>
+
+                        <div className="container-pick-and-drop">
+
+                            <div className="container-inputs container-input-radio">
+                                <input type="radio" name="drop" id="drop"/>
+                                <label htmlFor="drop">Drop - Off</label>
+                            </div>
+
+                            <div className="container-inputs">
+                                <label htmlFor="dropoff-location">Localização</label>
+                                <select id="dropoff-location" name="dropoff-location">
+                                    <option value="">Selecione sua cidade</option>
+                                </select>
+                            </div>
+
+                            <div className="container-inputs">
+                                <label htmlFor="dropoff-time">Horário</label>
+                                <input type="time" id="dropoff-time" name="dropoff-time"/>
+                            </div>
+
+                            <div className="container-inputs">
+                                <label htmlFor="dropoff-date">Data</label>
+                                <input type="date" id="dropoff-date" name="dropoff-date"/>
+                            </div>
+                        </div>
+                    </form>
                 </section>
 
-                <section aria-labelledby="payment-method">
-                    <h2 id="payment-method">Payment Method</h2>
-                    <p>Por favor, insira seu método de pagamento.</p>
-                    <form>
-                        <fieldset>
-                            <legend>Cartão de Crédito</legend>
-                            <label >Número do Cartão</label>
-                            <input type="text" id="card-number" name="card-number" placeholder="Número do cartão" required />
+                <section className="container-steps">
 
-                            <label >Titular do Cartão</label>
-                            <input type="text" id="card-holder" name="card-holder" placeholder="Nome do titular" required />
+                    <div className="header-step">
+                        <div className="header-form">
+                            <h2 className="title">Payment Method</h2>
+                            <span className="subtitle">Please enter your payment method</span>
+                        </div>
 
-                            <label >Data de Expiração</label>
-                            <input type="text" id="expiration" name="expiration" placeholder="MM/AA" required />
+                        <span className="steps">Step 3 of 4</span>
+                    </div>
 
-                            <label>CVC</label>
-                            <input type="text" id="cvc" name="cvc" placeholder="CVC" required />
+                    <form className="form-card">
+                        <div>
+                            <div className="container-inputs container-input-radio">
+                                <input type="radio" name="pick" id="pick"/>
+                                <label htmlFor="pick">Credit Card</label>
+                            </div>
 
-                        </fieldset>
-                        <fieldset>
-                            <legend>Outros Métodos</legend>
-                            <label>
-                                <input type="radio" name="payment" value="paypal" /> PayPal
-                            </label>
-                            <label>
-                                <input type="radio" name="payment" value="bitcoin" /> Bitcoin
-                            </label>
-                        </fieldset>
+                            <div>
+                                <img src={visa} alt="logo visa" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="card-number">Card Number</label>
+                            <input type="text" id="card-number" name="card-number" placeholder="Card Number"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="card-holder">Card Holder</label>
+                            <input type="text" id="card-holder" name="card-holder" placeholder="Card Holder"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="expiration">Expiration Date</label>
+                            <input type="text" id="expiration" name="expiration" placeholder="DD/MM/YY"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="cvc">CVC</label>
+                            <input type="text" id="cvc" name="cvc" placeholder="CVC"/>
+                        </div>
+
                     </form>
+
+                    <form className="form-card">
+                        <div>
+                            <div className="container-inputs container-input-radio">
+                                <input type="radio" name="pick" id="pick"/>
+                                <label htmlFor="pick">PayPal</label>
+                            </div>
+
+                            <div>
+                                <img src={paypal} alt="logo Paypal" />
+                            </div>
+                        </div>
+                    </form>
+
+                    <form className="form-card">
+                        <div>
+                            <div className="container-inputs container-input-radio">
+                                <input type="radio" name="pick" id="pick"/>
+                                <label htmlFor="pick">Bitcoin</label>
+                            </div>
+
+                            <div>
+                                <img src={bitcoin} alt="logo Bitcoin" />
+                            </div>
+                        </div>
+                    </form>
+
+
                 </section>
 
 
@@ -159,4 +255,4 @@ const Payment = () => {
     )
 }
 
-                        export default Payment
+export default Payment
