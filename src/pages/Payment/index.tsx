32cca8f60ab9ffe03e7a901cc6f3ff64a1eec794
zzmarques <walacemarqs@gmail.com>
2025-03-28 09:38:40 -0300

    fazendo responsivo da page payment

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -79,193 +79,206 @@ const Payment = () => {
 
                 </div>
 
-                <Steps>
-                    <StepHeader 
-                        title="Billing Info" 
-                        subtitle="Please enter your billing info" 
-                        step="Step 1 of 4"
-                    />
+                <div className="container-forms">
+                    <Steps>
+                        <StepHeader 
+                            title="Billing Info" 
+                            subtitle="Please enter your billing info" 
+                            step="Step 1 of 4"
+                        />
 
-                    <form className="form-step-1">
-                        <div className="container-inputs">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" id="name" name="name" placeholder="Your name"/>
-                        </div>
-                        
-                        <div className="container-inputs">
-                            <label htmlFor="address">Address</label>
-                            <input type="text" id="address" name="address" placeholder="Address"/>
-                        </div>
-                        
-                        <div className="container-inputs">
-                            <label htmlFor="phone">Phone Number</label>
-                            <input type="tel" id="phone" name="phone" placeholder="Phone Number"/>
-                        </div>
-                        
-                        <div className="container-inputs">
-                            <label htmlFor="city">Town/city</label>
-                            <input type="text" id="city" name="city" placeholder="Town or city"/>
-                        </div>
-                    </form>
-                </Steps>
-
-                <Steps>
-                    <StepHeader 
-                        title="Rental Info" 
-                        subtitle="Please select your rental date" 
-                        step="Step 2 of 4"
-                    />
-
-                    <form className="form-stap-2">
-
-                        <div className="container-pick-and-drop">
-
-                            <div className="container-inputs container-input-radio">
-                                <input type="radio" name="pick" id="pick"/>
-                                <label htmlFor="pick">Pick - Up</label>
-                            </div>
-
+                        <form className="form-step-1">
                             <div className="container-inputs">
-                                <label htmlFor="pickup-location">Localização</label>
-                                <select id="pickup-location" name="pickup-location">
-                                    <option value="">Selecione sua cidade</option>
-                                </select>
+                                <label htmlFor="name">Name</label>
+                                <input type="text" id="name" name="name" placeholder="Your name"/>
                             </div>
-
+                            
                             <div className="container-inputs">
-                                <label htmlFor="pickup-time">Horário</label>
-                                <input type="time" id="pickup-time" name="pickup-time"/>
+                                <label htmlFor="address">Address</label>
+                                <input type="text" id="address" name="address" placeholder="Address"/>
                             </div>
-
+                            
                             <div className="container-inputs">
-                                <label htmlFor="pickup-date">Data</label>
-                                <input type="date" id="pickup-date" name="pickup-date"/>
+                                <label htmlFor="phone">Phone Number</label>
+                                <input type="tel" id="phone" name="phone" placeholder="Phone Number"/>
                             </div>
-                        </div>
-
-                        <div className="container-pick-and-drop">
-
-                            <div className="container-inputs container-input-radio">
-                                <input type="radio" name="drop" id="drop"/>
-                                <label htmlFor="drop">Drop - Off</label>
-                            </div>
-
+                            
                             <div className="container-inputs">
-                                <label htmlFor="dropoff-location">Localização</label>
-                                <select id="dropoff-location" name="dropoff-location">
-                                    <option value="">Selecione sua cidade</option>
-                                </select>
+                                <label htmlFor="city">Town/city</label>
+                                <input type="text" id="city" name="city" placeholder="Town or city"/>
+                            </div>
+                        </form>
+                    </Steps>
+
+                    <Steps>
+                        <StepHeader 
+                            title="Rental Info" 
+                            subtitle="Please select your rental date" 
+                            step="Step 2 of 4"
+                        />
+
+                        <form className="form-stap-2">
+
+                            <div className="container-pick-and-drop">
+
+                                <div className="container-inputs container-input-radio">
+                                    <input type="radio" name="pick" id="pick"/>
+                                    <label htmlFor="pick">Pick - Up</label>
+                                </div>
+
+                                <div className="content-inputs">
+
+                                    <div className="container-inputs">
+                                        <label htmlFor="pickup-location">Localização</label>
+                                        <select id="pickup-location" name="pickup-location">
+                                            <option value="">Selecione sua cidade</option>
+                                        </select>
+                                    </div>
+
+                                    <div className="container-inputs">
+                                        <label htmlFor="pickup-time">Horário</label>
+                                        <input type="time" id="pickup-time" name="pickup-time"/>
+                                    </div>
+
+                                    <div className="container-inputs">
+                                        <label htmlFor="pickup-date">Data</label>
+                                        <input type="date" id="pickup-date" name="pickup-date"/>
+                                    </div>
+
+                                </div>
                             </div>
 
-                            <div className="container-inputs">
-                                <label htmlFor="dropoff-time">Horário</label>
-                                <input type="time" id="dropoff-time" name="dropoff-time"/>
+                            <div className="container-pick-and-drop">
+
+                                <div className="container-inputs container-input-radio">
+                                    <input type="radio" name="drop" id="drop"/>
+                                    <label htmlFor="drop">Drop - Off</label>
+                                </div>
+
+                                <div className="content-inputs">
+
+                                    <div className="container-inputs">
+                                        <label htmlFor="dropoff-location">Localização</label>
+                                        <select id="dropoff-location" name="dropoff-location">
+                                            <option value="">Selecione sua cidade</option>
+                                        </select>
+                                    </div>
+
+                                    <div className="container-inputs">
+                                        <label htmlFor="dropoff-time">Horário</label>
+                                        <input type="time" id="dropoff-time" name="dropoff-time"/>
+                                    </div>
+
+                                    <div className="container-inputs">
+                                        <label htmlFor="dropoff-date">Data</label>
+                                        <input type="date" id="dropoff-date" name="dropoff-date"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </Steps>
+
+                    <Steps>
+                        <StepHeader 
+                            title="Payment Method" 
+                            subtitle="Please enter your payment method" 
+                            step="Step 3 of 4"
+                        />
+
+                        <form className="form-step-3 active">
+                            <div className="pag">
+                                <div className="container-input-radio">
+                                    <input type="radio" name="pick-pg" id="pick-pg"/>
+                                    <label htmlFor="pick-pg">Credit Card</label>
+                                </div>
+
+                                <div>
+                                    <img src={visa} alt="logo visa" />
+                                </div>
                             </div>
 
-                            <div className="container-inputs">
-                                <label htmlFor="dropoff-date">Data</label>
-                                <input type="date" id="dropoff-date" name="dropoff-date"/>
+                            {/* <div className="content-input-steps-3">
+
+                                <div className="container-inputs">
+                                    <label htmlFor="card-number">Card Number</label>
+                                    <input type="text" id="card-number" name="card-number" placeholder="Card Number"/>
+                                </div>
+
+                                <div className="container-inputs">
+                                    <label htmlFor="card-holder">Card Holder</label>
+                                    <input type="text" id="card-holder" name="card-holder" placeholder="Card Holder"/>
+                                </div>
+
+                                <div className="container-inputs">
+                                    <label htmlFor="expiration">Expiration Date</label>
+                                    <input type="text" id="expiration" name="expiration" placeholder="DD/MM/YY"/>
+                                </div>
+
+                                <div className="container-inputs">
+                                    <label htmlFor="cvc">CVC</label>
+                                    <input type="text" id="cvc" name="cvc" placeholder="CVC"/>
+                                </div>
+
+                            </div> */}
+
+                        </form>
+
+                        <form className="form-step-3">
+                            <div className="pag">
+                                <div className="container-input-radio">
+                                    <input type="radio" name="pick-pg" id="pick-pg"/>
+                                    <label htmlFor="pick-pg">PayPal</label>
+                                </div>
+
+                                <div>
+                                    <img src={paypal} alt="logo Paypal" />
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </Steps>
+                        </form>
 
-                <Steps>
-                    <StepHeader 
-                        title="Payment Method" 
-                        subtitle="Please enter your payment method" 
-                        step="Step 3 of 4"
-                    />
+                        <form className="form-step-3">
+                            <div className="pag">
+                                <div className="container-input-radio">
+                                    <input type="radio" name="pick-pg" id="pick-pg"/>
+                                    <label htmlFor="pick-pg">Bitcoin</label>
+                                </div>
 
-                    <form className="form-step-3">
-                        <div className="pag">
-                            <div className="container-input-radio">
-                                <input type="radio" name="pick-pg" id="pick-pg"/>
-                                <label htmlFor="pick-pg">Credit Card</label>
+                                <div>
+                                    <img src={bitcoin} alt="logo Bitcoin" />
+                                </div>
                             </div>
+                        </form>
+                    </Steps>
 
-                            <div>
-                                <img src={visa} alt="logo visa" />
-                            </div>
-                        </div>
+                    <Steps>
+                        <StepHeader 
+                            title="Confirmation" 
+                            subtitle="We are getting to the end. Just few
+                            clicks and your rental is ready!" 
+                            step="Step 4 of 4"
+                        />
 
-                        {/* <div className="container-inputs">
-                            <label htmlFor="card-number">Card Number</label>
-                            <input type="text" id="card-number" name="card-number" placeholder="Card Number"/>
-                        </div>
-
-                        <div className="container-inputs">
-                            <label htmlFor="card-holder">Card Holder</label>
-                            <input type="text" id="card-holder" name="card-holder" placeholder="Card Holder"/>
-                        </div>
-
-                        <div className="container-inputs">
-                            <label htmlFor="expiration">Expiration Date</label>
-                            <input type="text" id="expiration" name="expiration" placeholder="DD/MM/YY"/>
-                        </div>
-
-                        <div className="container-inputs">
-                            <label htmlFor="cvc">CVC</label>
-                            <input type="text" id="cvc" name="cvc" placeholder="CVC"/>
-                        </div> */}
-
-                    </form>
-
-                    <form className="form-step-3">
-                        <div className="pag">
-                            <div className="container-input-radio">
-                                <input type="radio" name="pick-pg" id="pick-pg"/>
-                                <label htmlFor="pick-pg">PayPal</label>
-                            </div>
-
-                            <div>
-                                <img src={paypal} alt="logo Paypal" />
-                            </div>
-                        </div>
-                    </form>
-
-                    <form className="form-step-3">
-                        <div className="pag">
-                            <div className="container-input-radio">
-                                <input type="radio" name="pick-pg" id="pick-pg"/>
-                                <label htmlFor="pick-pg">Bitcoin</label>
+                        <form className="form-step-4">
+                            <div className="container-checkbox">
+                                <input type="checkbox" name="termo-1" id="termo-1" />
+                                <label htmlFor="termo-1">I agree with sending an Marketing and newsletter emails. No spam, promissed!</label>
                             </div>
 
-                            <div>
-                                <img src={bitcoin} alt="logo Bitcoin" />
+                            <div className="container-checkbox">
+                                <input type="checkbox" name="termo-2" id="termo-2" />
+                                <label htmlFor="termo-2">I agree with our <span>terms and conditions</span> and <span>privacy policy</span>!</label>
                             </div>
+                            
+                            <button className="comfirm">Rental Now</button>
+                        </form>
+
+                        <div className="container-sf">
+                            <img src={sf} alt="security-safety"/>
+                            <span className="title">All your data are safe</span>
+                            <span className="subtitle">We are using the most advanced security to provide you the best experience ever.</span>
                         </div>
-                    </form>
-                </Steps>
-
-                <Steps>
-                    <StepHeader 
-                        title="Confirmation" 
-                        subtitle="We are getting to the end. Just few
-                        clicks and your rental is ready!" 
-                        step="Step 4 of 4"
-                    />
-
-                    <form className="form-step-4">
-                        <div className="container-checkbox">
-                            <input type="checkbox" name="termo-1" id="termo-1" />
-                            <label htmlFor="termo-1">I agree with sending an Marketing and newsletter emails. No spam, promissed!</label>
-                        </div>
-
-                        <div className="container-checkbox">
-                            <input type="checkbox" name="termo-2" id="termo-2" />
-                            <label htmlFor="termo-2">I agree with our <span>terms and conditions</span> and <span>privacy policy</span>!</label>
-                        </div>
-                        
-                        <button className="comfirm">Rental Now</button>
-                    </form>
-
-                    <div className="container-sf">
-                        <img src={sf} alt="security-safety"/>
-                        <span className="title">All your data are safe</span>
-                        <span className="subtitle">We are using the most advanced security to provide you the best experience ever.</span>
-                    </div>
-                </Steps>
+                    </Steps>
+                </div>
             </section>
         </main>
     )
